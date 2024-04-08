8076c25062b1f9f7de911471dfdef32da215ead4
Gabriel Holmes <gabrielholmes2002@gmail.com>
2024-04-08 14:13:02 +0000

    adding all the content for the edit and delete buttons

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-gabrielh02-djangobloggh-3cswc76nvec.ws-eu110.gitpod.io', '.herokuapp.com']
 
 
